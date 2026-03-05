ac32bba97c14ec01ec42a8812ec39b6c4d304c6a
Dhinakar29 <166849695+Dhinakar29@users.noreply.github.com>
2026-03-05 15:34:33 +0530

    added new scripts

---
+++ getCall.spec.js
@@ -0,0 +1,36 @@
+import {test,expect} from '@playwright/test'
+
+test.skip("get call request", async ({request}) => {
+
+    const resp = await request.get("https://dummy.restapiexample.com/api/v1/employees/")
+    
+    const respBody = await resp.body()
+    //console.log(respBody);
+    const respStatus = resp.status()
+    //console.log(respStatus);
+    const respStatusText = resp.statusText()
+    console.log(respStatus);
+    
+    //expect(respStatus).toBe(200)
+    
+    
+    // get the response as json body
+    const repjson = await resp.json()
+    //console.log(repjson)
+
+    //get the response as headers
+    const resHeaders = resp.headers()
+    //console.log(resHeaders)
+
+    //get the response of headersarray
+    const resHeadersArray = resp.headersArray()
+})
+
+test("get request", async ({request}) => {
+   const resp = await request.get("https://dummy.restapiexample.com/api/v1/employees/")
+   const status = resp.status()
+
+   console.log(status);
+   
+   
+})
